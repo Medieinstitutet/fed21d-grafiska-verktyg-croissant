0b2eabe7538ff21f8ea439a29c415a22c0f29851
Magnus Vahlström <Wahlstrommm@gmail.com>
2022-09-22 17:02:39 +0200

    started on the meny overaly, script almost done...

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 function CookieTextDown() {
-  var cookieContainer = document.getElementById('cookie-container');
-
+  let cookieContainer = document.getElementById('cookie-container');
   cookieContainer.style.display = 'none';
-
-  console.log('Tjena, tjena');
 }
+
+//Inte klar än!
+
+let hambugermeny = document.getElementById('hamburger-menu');
+let toogle = true;
+let menuOverlay = document.getElementById('menu-overlay');
+let hamburgerSpans = document.querySelectorAll('span.hamburger-spans');
+hambugermeny.addEventListener('click', () => {
+  hambugermeny.innerHTML = '';
+  let image = document.createElement('img');
+  image.src = './assets/cross.svg';
+  console.log('click');
+
+  hamburgerSpans[0].style.display = 'none';
+  menuOverlay.style.display = 'none';
+  hambugermeny.append(image);
+});
